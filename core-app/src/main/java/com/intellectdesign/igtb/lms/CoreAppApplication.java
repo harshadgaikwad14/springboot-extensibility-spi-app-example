5f2ce30eb8c6720451a6672771664bfa19ed2355
harshadgaikwad14 <harshadgaikwad14@gmail.com>
2020-04-30 16:04:06 +0530

    sweep structure code added

--- core-app/src/main/java/com/intellectdesign/igtb/lms/CoreAppApplication.java
+++ core-app/src/main/java/com/intellectdesign/igtb/lms/CoreAppApplication.java
@@ -1,14 +1,7 @@
 package com.intellectdesign.igtb.lms;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.serviceloader.ServiceListFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.intellectdesign.igtb.lms.ExTaxTypeSpi;
 
 @SpringBootApplication
 public class CoreAppApplication {
@@ -26,30 +19,6 @@ public class CoreAppApplication {
 	 * 
 	 */
 
-	@Bean
-	public List<ExTaxTypeSpi> exTaxTypeSpiService() {
-		System.out.println("Crating proxy for exTaxTypeSpiService implementations");
-
-		List<ExTaxTypeSpi> exTaxTypeSpis = getExTaxTypeSpiServices();
-		System.out.println("Found exTaxTypeSpiService implementations" + exTaxTypeSpis.size());
-
-		return exTaxTypeSpis;// createProxiedNotificationSendersAsSpringWillNotCreateProxyForThese(senders);
-	}
-
-	private List<ExTaxTypeSpi> getExTaxTypeSpiServices() {
-		List<ExTaxTypeSpi> senders = new ArrayList<>();
-		try {
-			ServiceListFactoryBean serviceListFactoryBean = new ServiceListFactoryBean();
-			serviceListFactoryBean.setServiceType(ExTaxTypeSpi.class);
-			serviceListFactoryBean.afterPropertiesSet();
-
-			senders = (List<ExTaxTypeSpi>) serviceListFactoryBean.getObject();
-		} catch (Exception ex) {
-			System.out.println("Unable to retrieve ExTaxTypeSpi implementations" + ex.getMessage());
-			ex.printStackTrace();
-		}
-
-		return senders;
-	}
+	
 
 }
